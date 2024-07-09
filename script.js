62a6f5214450fc25a07491b6067184b5ce36491f
Stas <tim4chenko.stas@gmail.com>
2024-07-09 23:26:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "john",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person["age"]);
+
+let array = ['plum.png', 'orange.png', 'bebra.png'];
+
+for(let i = 0; i<array.length; i++) {
+    console.log(array[i]);
+}
